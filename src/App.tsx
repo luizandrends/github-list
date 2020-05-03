2c0dc3b8ea48fced47fee18e5fed8f46a1b5580d
Luiz Andre Nunes da Silva <luizandrenunesdasilva@Luizs-MacBook-Pro.local>
2020-05-02 22:32:27 -0300

    Feature: Creating global style and background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
 import Routes from './routes/index';
 
 const App: React.FC = () => (
   <BrowserRouter>
+    <GlobalStyle />
     <Routes />
   </BrowserRouter>
 );
